b85e1f57a5d9c50ed89b1066ae8d8d05f26991a5
Emirhan Akpınar <leetpocketmine@gmail.com>
2021-12-19 23:15:18 +0300

    redesigned movie details on Card component

--- components/Card.js
+++ components/Card.js
@@ -1,19 +1,13 @@
 const Card = props => {
-    let voteClasses = "w-auto px-4 max-w-max rounded-full shadow-sm flex items-center justify-center text-sm font-semibold mb-2";
-
-    if (props.vote > 7){
-        voteClasses += " bg-green-100 text-green-500";
-    }else{
-        voteClasses += " bg-yellow-100 text-yellow-500";
-    }
+    let voteClasses = `w-auto px-4 max-w-max rounded-full shadow-sm flex items-center justify-center text-sm font-semibold mb-2 ${props.vote > 7 ? "bg-green-100 text-green-500" : "bg-yellow-100 text-yellow-500"}`;
 
     return (
         <div className="w-[24rem] h-[28rem] rounded-xl shadow-lg cursor-pointer hover:shadow-xl transition-shadow">
             <img className="rounded-t-lg shadow-md" src={`https://www.themoviedb.org/t/p/original${props.thumbnail}`} alt={props.title}/>
             <div className="w-full flex flex-col p-6">
-                <span className={voteClasses}>{props.vote + (props.vote.toString().length === 1 ? ".0" : "")}</span>
+                <span className={voteClasses}>{`${props.vote}${props.vote.toString().length === 1 ? ".0" : ""}`}</span>
                 <h3 className="text-xl font-bold mb-4">{props.title}</h3>
-                <p className="text-gray-400 max-h-64 text-sm text-clip">{props.details}</p>
+                <p className="text-gray-400 max-h-64 text-sm text-clip overflow-hidden line-clamp-6 md:line-clamp-5">{props.details}</p>
             </div>
         </div>
     )
