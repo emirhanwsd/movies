add138df78eb00a019daa779d4012f84187c0e0c
Emirhan Akpınar <leetpocketmine@gmail.com>
2021-12-19 19:01:24 +0300

    redesigned Card component

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,19 @@
 const Card = props => {
+    let voteClasses = "w-auto px-4 max-w-max rounded-full shadow-sm flex items-center justify-center text-sm font-semibold mb-2";
+
+    if (props.vote > 7){
+        voteClasses += " bg-green-100 text-green-500";
+    }else{
+        voteClasses += " bg-yellow-100 text-yellow-500";
+    }
+
     return (
         <div className="w-[24rem] h-[28rem] rounded-xl shadow-lg cursor-pointer hover:shadow-xl transition-shadow">
-            <img className="rounded-t-lg shadow-md" src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/9OYu6oDLIidSOocW3JTGtd2Oyqy.jpg" alt="The Good Doctor"/>
+            <img className="rounded-t-lg shadow-md" src={`https://www.themoviedb.org/t/p/original${props.thumbnail}`} alt={props.title}/>
             <div className="w-full flex flex-col p-6">
-                <span className="w-auto px-4 max-w-max rounded-full shadow-sm flex items-center justify-center bg-indigo-100 text-indigo-500 text-sm font-semibold mb-2">Horror</span>
-                <h3 className="text-xl font-bold mb-4">The Good Doctor</h3>
-                <p className="text-gray-400 text-sm text-clip">
-                    Shaun Murphy, a young surgeon with autism and savant syndrome, relocates from a quiet country life to join a prestigious hospital's surgical unit. Unable to personally connect with those around him, Shaun uses his extraordinary medical gifts to save lives and challenge the skepticism of his colleagues.
-                </p>
+                <span className={voteClasses}>{props.vote + (props.vote.toString().length === 1 ? ".0" : "")}</span>
+                <h3 className="text-xl font-bold mb-4">{props.title}</h3>
+                <p className="text-gray-400 max-h-64 text-sm text-clip">{props.details}</p>
             </div>
         </div>
     )
